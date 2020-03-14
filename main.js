4029aeb47a482b1537810471db67128c216a6ec3
Puripant <puripant@gmail.com>
2020-03-14 20:48:21 +0700

    Center nodes without forceCenter()

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const width = 600;
+const width = 400;
 const height = 600;
 
 const drag = simulation => {
@@ -20,9 +20,9 @@ const drag = simulation => {
   }
   
   return d3.drag()
-      .on("start", dragstarted)
-      .on("drag", dragged)
-      .on("end", dragended);
+    .on("start", dragstarted)
+    .on("drag", dragged)
+    .on("end", dragended);
 }
 
 const corona_num = 20;
@@ -41,7 +41,11 @@ const simulation = d3.forceSimulation(nodes)
   .force("charge", d3.forceManyBody()
     .strength(-10)
   )
-  .force("center", d3.forceCenter(width / 2, height / 2));
+  .force("collide", d3.forceCollide()
+    .strength(1)
+    .radius(15)
+  )
+  // .force("center", d3.forceCenter(width / 2, height / 2));
 
 const svg = d3.select("svg");
 
@@ -64,12 +68,12 @@ const node = svg.append("g")
 
 simulation.on("tick", () => {
   link
-    .attr("x1", d => d.source.x)
-    .attr("y1", d => d.source.y)
-    .attr("x2", d => d.target.x)
-    .attr("y2", d => d.target.y);
+    .attr("x1", d => d.source.x + width / 2)
+    .attr("y1", d => d.source.y + height / 2)
+    .attr("x2", d => d.target.x + width / 2)
+    .attr("y2", d => d.target.y + height / 2);
 
   node
-    .attr("cx", d => d.x)
-    .attr("cy", d => d.y);
+    .attr("cx", d => d.x + width / 2)
+    .attr("cy", d => d.y + height / 2);
 });
